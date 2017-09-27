0efd78221d4164610d4b2559f6c82cc6cdb16cfe
susan31213 <susan31213@gmail.com>
2017-09-28 00:15:25 +0800

    Delete ScanActivity, update zxing library to 3.3.0, speed up scanning.

--- app/src/main/java/com/example/susan/scanner/MainActivity.java
+++ app/src/main/java/com/example/susan/scanner/MainActivity.java
@@ -1,59 +1,76 @@
 package com.example.susan.scanner;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int EDIT=1;
+    private TextView scanResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView scanResult = (TextView)findViewById(R.id.textView3);
+        scanResult = (TextView)findViewById(R.id.textView3);
         scanResult.setText("");
 
         Button startBtn = (Button)findViewById(R.id.scanner);
+        final Activity activity = this;
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, ScanActivity.class);
-                startActivityForResult(intent, EDIT);
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
             }
         });
     }
 
     @Override
-    public void onStart()
-    {
-        super.onStart();
-
-
-
-
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch(requestCode){
-            case EDIT:
-                if(resultCode == RESULT_OK) {
-                    TextView scanResult = (TextView)findViewById(R.id.textView3);
-                    Bundle scanBundle = data.getExtras();
-                    String setText = scanBundle.getString("result") + "\n";
-                    scanResult.append(setText);
-                }
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null)
+        {
+            if(result.getContents() == null)
+            {
+                Toast.makeText(this, "You cancelled the scanning.", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                String content = result.getContents();
+                Toast.makeText(this, content, Toast.LENGTH_LONG).show();
+                scanResult.append(content + "\n");
+            }
         }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+//        switch(requestCode){
+//            case EDIT:
+//                if(resultCode == RESULT_OK) {
+//                    TextView scanResult = (TextView)findViewById(R.id.textView3);
+//                    Bundle scanBundle = data.getExtras();
+//                    String setText = scanBundle.getString("result") + "\n";
+//                    scanResult.append(setText);
+//                }
+//        }
     }
 
 }
